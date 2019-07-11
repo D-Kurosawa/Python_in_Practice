fd98fb201c0dc4b4e7e323ec220ed2046be6d594
Daisuke Kurosawa <dai-kuro@sings.jp>
2019-07-11 20:41:11 +0900

    [replace] " --> '

--- sect_1-2_Builder/formbuilder.py
+++ sect_1-2_Builder/formbuilder.py
@@ -27,32 +27,32 @@ else:
 
 
 def main():
-    if len(sys.argv) > 1 and sys.argv[1] == "-P":  # For regression testing
+    if len(sys.argv) > 1 and sys.argv[1] == '-P':  # For regression testing
         print(create_login_form(HtmlFormBuilder()))
         print(create_login_form(TkFormBuilder()))
         return
 
-    html_filename = os.path.join(tempfile.gettempdir(), "login.html")
+    html_filename = os.path.join(tempfile.gettempdir(), 'login.html')
     html_form = create_login_form(HtmlFormBuilder())
-    with open(html_filename, "w", encoding="utf-8") as file:
+    with open(html_filename, 'w', encoding='utf-8') as file:
         file.write(html_form)
-    print("wrote", html_filename)
+    print('wrote', html_filename)
 
-    tk_filename = os.path.join(tempfile.gettempdir(), "login.py")
+    tk_filename = os.path.join(tempfile.gettempdir(), 'login.py')
     tk_form = create_login_form(TkFormBuilder())
-    with open(tk_filename, "w", encoding="utf-8") as file:
+    with open(tk_filename, 'w', encoding='utf-8') as file:
         file.write(tk_form)
-    print("wrote", tk_filename)
+    print('wrote', tk_filename)
 
 
 def create_login_form(builder):
-    builder.add_title("Login")
-    builder.add_label("Username", 0, 0, target="username")
-    builder.add_entry("username", 0, 1)
-    builder.add_label("Password", 1, 0, target="password")
-    builder.add_entry("password", 1, 1, kind="password")
-    builder.add_button("Login", 2, 0)
-    builder.add_button("Cancel", 2, 1)
+    builder.add_title('Login')
+    builder.add_label('Username', 0, 0, target='username')
+    builder.add_entry('username', 0, 1)
+    builder.add_label('Password', 1, 0, target='password')
+    builder.add_entry('password', 1, 1, kind='password')
+    builder.add_button('Login', 2, 0)
+    builder.add_button('Cancel', 2, 1)
     return builder.form()
 
 
@@ -94,37 +94,38 @@ class HtmlFormBuilder(AbstractFormBuilder):
         super().add_title(escape(title))
 
     def add_label(self, text, row, column, **kwargs):
-        self.items[(row, column)] = ('<td><label for="{}">{}:</label></td>'
-                                     .format(kwargs["target"], escape(text)))
+        self.items[(row, column)] = (
+            '<td><label for="{}">{}:</label></td>'.format(kwargs['target'],
+                                                          escape(text)))
 
     def add_entry(self, variable, row, column, **kwargs):
-        html = """<td><input name="{}" type="{}" /></td>""".format(
-            variable, kwargs.get("kind", "text"))
+        html = '<td><input name="{}" type="{}" /></td>'.format(
+            variable, kwargs.get('kind', 'text'))
         self.items[(row, column)] = html
 
     def add_button(self, text, row, column, **kwargs):
-        html = """<td><input type="submit" value="{}" /></td>""".format(
+        html = '<td><input type="submit" value="{}" /></td>'.format(
             escape(text))
         self.items[(row, column)] = html
 
     def form(self):
-        html = ["<!doctype html>\n<html><head><title>{}</title></head>"
-                "<body>".format(self.title), '<form><table border="0">']
+        html = ['<!doctype html>\n<html><head><title>{}</title></head>'
+                '<body>'.format(self.title), '<form><table border="0">']
         this_row = None
         for key, value in sorted(self.items.items()):
             row, column = key
             if this_row is None:
-                html.append("  <tr>")
+                html.append('  <tr>')
             elif this_row != row:
-                html.append("  </tr>\n  <tr>")
+                html.append('  </tr>\n  <tr>')
             this_row = row
-            html.append("    " + value)
-        html.append("  </tr>\n</table></form></body></html>")
-        return "\n".join(html)
+            html.append('    ' + value)
+        html.append('  </tr>\n</table></form></body></html>')
+        return '\n'.join(html)
 
 
 class TkFormBuilder(AbstractFormBuilder):
-    TEMPLATE = """#!/usr/bin/env python3
+    TEMPLATE = '''#!/usr/bin/env python3
 import tkinter as tk
 import tkinter.ttk as ttk
 
@@ -143,16 +144,16 @@ class {name}Form(tk.Toplevel):
         self.grab_set()
         self.wait_window(self)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     application = tk.Tk()
     window = {name}Form(application)
     application.protocol("WM_DELETE_WINDOW", application.quit)
     application.mainloop()
-"""
+'''
 
     def __init__(self):
         super().__init__()
-        self.title = "TkFormBuilder"
+        self.title = 'TkFormBuilder'
         self.statements = []
 
     def add_title(self, title):
@@ -160,42 +161,41 @@ if __name__ == "__main__":
 
     def add_label(self, text, row, column, **kwargs):
         name = self._canonicalize(text)
-        create = """self.{}Label = ttk.Label(self, text="{}:")""".format(
+        create = 'self.{}Label = ttk.Label(self, text="{}:")'.format(
             name, text)
-        layout = """self.{}Label.grid(row={}, column={}, sticky=tk.W, \
-padx="0.75m", pady="0.75m")""".format(name, row, column)
+        layout = 'self.{}Label.grid(row={}, column={}, sticky=tk.W, \
+padx="0.75m", pady="0.75m")'.format(name, row, column)
         self.statements.extend((create, layout))
 
     def add_entry(self, variable, row, column, **kwargs):
         name = self._canonicalize(variable)
-        extra = "" if kwargs.get("kind") != "password" else ', show="*"'
-        create = "self.{}Entry = ttk.Entry(self{})".format(name, extra)
-        layout = """self.{}Entry.grid(row={}, column={}, sticky=(\
-tk.W, tk.E), padx="0.75m", pady="0.75m")""".format(name, row, column)
+        extra = '' if kwargs.get('kind') != 'password' else ', show="*"'
+        create = 'self.{}Entry = ttk.Entry(self{})'.format(name, extra)
+        layout = 'self.{}Entry.grid(row={}, column={}, sticky=(\
+tk.W, tk.E), padx="0.75m", pady="0.75m")'.format(name, row, column)
         self.statements.extend((create, layout))
 
     def add_button(self, text, row, column, **kwargs):
         name = self._canonicalize(text)
-        create = ("""self.{}Button = ttk.Button(self, text="{}")"""
+        create = ('self.{}Button = ttk.Button(self, text="{}")'
                   .format(name, text))
-        layout = """self.{}Button.grid(row={}, column={}, padx="0.75m", \
-pady="0.75m")""".format(name, row, column)
+        layout = 'self.{}Button.grid(row={}, column={}, padx="0.75m", \
+pady="0.75m")'.format(name, row, column)
         self.statements.extend((create, layout))
 
     def form(self):
-        return TkFormBuilder.TEMPLATE.format(title=self.title,
-                                             name=self._canonicalize(
-                                                 self.title, False),
-                                             statements="\n        ".join(
-                                                 self.statements))
+        return TkFormBuilder.TEMPLATE.format(
+            title=self.title,
+            name=self._canonicalize(self.title, False),
+            statements='\n        '.join(self.statements))
 
     @staticmethod
     def _canonicalize(text, start_lower=True):
-        text = re.sub(r"\W+", "", text)
+        text = re.sub(r'\W+', '', text)
         if text[0].isdigit():
-            return "_" + text
+            return '_' + text
         return text if not start_lower else text[0].lower() + text[1:]
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
